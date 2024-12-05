7689416024facbe1f1424a7999b3f1c3374ddac5
keruzam <mazurek.zg@gmail.com>
2024-12-05 18:28:17 +0100

    check

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,11 @@ class MainApp extends StatelessWidget {
             Container(
                 color: Colors.grey.shade600,
                 child: Row(
-                children: [
-                  Container(padding: EdgeInsets.all(10.0), width: 40, child: Icon(Icons.wb_sunny_rounded, size: 32, color: Colors.green)),
-                  Container(margin: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0), width: 300, child: Text('Mobile - mobile.firmino.pl', style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: Colors.white))),
-                ],
-            )
-
+                  children: [
+                    Container(padding: EdgeInsets.all(10.0), width: 40, child: Icon(Icons.wb_sunny_rounded, size: 32, color: Colors.green)),
+                    Container(margin: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0), width: 300, child: Text('Mobile - mobile.firmino.pl', style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: Colors.white))),
+                  ],
+                )
             )
           ],
         ),
